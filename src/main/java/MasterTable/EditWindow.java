e3717bc4518b0819a004a0280a56e4851ccb944b
milos547 <mcvijetic151@gmail.com>
2026-04-29 14:41:15 +0200

    User Story 3 work in Prohress

--- src/main/java/MasterTable/EditWindow.java
+++ src/main/java/MasterTable/EditWindow.java
@@ -5,32 +5,60 @@ import java.awt.*;
 
 public class EditWindow extends JFrame {
 
-
+    private BasicTable model;
 
     public EditWindow() {
+        this.model = model;
         setTitle("Edit Hotel");
-        setSize(300, 300);
+        setSize(500, 400);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
 
 
-        setLayout(new GridLayout(5, 2, 10, 10));
+        setLayout(new GridLayout(7, 3, 12, 15));
 
-        add(new JLabel("Name:"));
-        add(new JTextField());
+        JTextField name = new JTextField();
+        JTextField category = new JTextField();
+        JTextField owner = new JTextField();
+        JTextField contact = new JTextField();
+        JTextField address = new JTextField();
+        JTextField city = new JTextField();
+        JTextField citycode = new JTextField();
+        JTextField phone = new JTextField();
+        JTextField rooms = new JTextField();
+        JTextField beds = new JTextField();
 
-        add(new JLabel("City:"));
-        add(new JTextField());
-
-        add(new JLabel("Rooms:"));
-        add(new JTextField());
-
-        add(new JLabel("Beds:"));
-        add(new JTextField());
+        add(new JLabel("Name:")); add(name);
+        add(new JLabel("Category:")); add(category);
+        add(new JLabel("Owner:")); add(owner);
+        add(new JLabel("Contact:")); add(contact);
+        add(new JLabel("Address:")); add(address);
+        add(new JLabel("City:")); add(city);
+        add(new JLabel("Citycode:")); add(citycode);
+        add(new JLabel("Phone:")); add(phone);
+        add(new JLabel("Number of Rooms:")); add(rooms);
+        add(new JLabel("Number of Beds:")); add(beds);
 
         JButton saveButton = new JButton("Save");
         add(saveButton);
 
+
+        saveButton.addActionListener(e -> {
+            Object[] newRow = {
+                    "AUTO",//ID automatic
+                    category.getText(),
+                    owner.getText(),
+                    contact.getText(),
+                    address.getText(),
+                    city.getText(),
+                    citycode.getText(),
+                    phone.getText(),
+                    rooms.getText(),
+                    beds.getText()
+            };
+
+            model.addRow(newRow);
+        });
 
 
         setVisible(true);
