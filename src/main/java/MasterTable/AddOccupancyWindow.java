d2b21707b7cc6c9c5c01eef022c22d410dc6870f
tekincakmak <tekin.cakmak05@gmail.com>
2026-05-05 12:14:44 +0200

    Added comments and finished US with Milan

--- src/main/java/MasterTable/AddOccupancyWindow.java
+++ src/main/java/MasterTable/AddOccupancyWindow.java
@@ -32,7 +32,23 @@ public class AddOccupancyWindow extends JFrame {
 
 
     }
+    private void addComponents() { //Columns
+        add(new JLabel("Hotel:"));
+        add(hotelSelect);
+        add(new JLabel("Year:"));
+        add(year);
+        add(new JLabel("Month:"));
+        add(month);
+        add(new JLabel("Room Occupancy %:"));
+        add(roomOcc);
+        add(new JLabel("Bed Occupancy %:"));
+        add(bedOcc);
 
+        // save button -> saveOccupancy
+        JButton saveButton = new JButton("Save");
+        add(saveButton);
+
+        saveButton.addActionListener(e -> saveOccupancy());
     }
 
     private void initComponents() { //each label gets one column
