4683ffeb8387295113fbc8cea25077270df44108
milos547 <mcvijetic151@gmail.com>
2026-06-14 00:03:08 +0200

    added SplashScreen for start to hide slow load time

--- src/main/java/MasterTable/Main.java
+++ src/main/java/MasterTable/Main.java
@@ -4,6 +4,8 @@ import MasterTable.gui.LoginWindow;
 import MasterTable.entity.user.UsersHibernate;
 import MasterTable.gui.MainWindow;
 import MasterTable.util.HibernateUtil;
+import MasterTable.gui.SplashScreen;
+
 
 import javax.swing.*;
 
@@ -11,16 +13,27 @@ public class Main {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() ->{
-            HibernateUtil.getSessionFactory(); //SessionFactory gets prepared so LogIn is faster
-            LoginWindow login = new LoginWindow();
-            UsersHibernate user = login.getLoggedInUser();
 
-            if(user != null){
-                new MainWindow(user).setVisible(true);
-            }else {
-                System.exit(0); // Window closes without Login
-            }
+            //Splash Screen
+            SplashScreen splash = new SplashScreen();
+            splash.setVisible(true);
+
+            new Thread(() -> {
+                HibernateUtil.getSessionFactory(); //SessionFactory gets prepared so LogIn is faster
+
+                SwingUtilities.invokeLater(() -> {
+                    splash.dispose();
+                    LoginWindow login = new LoginWindow();
+                    UsersHibernate user = login.getLoggedInUser();
+                    if(user != null){
+                        new MainWindow(user).setVisible(true);
+                    }else {
+                        System.exit(0); // Window closes without Login
+                    }
+
+
+                });
+            }).start();
         });
-
     }
 }
